0d5c144dd16384da4f20e3dbca2f1c818b926f2b
ZebraKK <yuxw_512@126.com>
2016-12-19 15:27:34 +0800

    1
    增加一个.c文件

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+
+}
+
+//list reverse
+
+typedef struct
+{
+    Item date;
+    Node *pnext;
+}Node;
+
+int reverse(Node *list)
+{
+    Node *p;
+    Node *q;
+
+    if (list->pnext != NULL)
+    {
+        p=list->pnext;
+
+    }
+    
+    while()
+    {
+    }
+}
+
